5bb292f5257b7a9627b3795bf34de2cd9c6c6314
Oren Zamir <orenzamir5@gmail.com>
2022-07-26 12:57:05 +0300

    Update runner.sh

--- .github/scripts/runner.sh
+++ .github/scripts/runner.sh
@@ -4,4 +4,4 @@ env="$1"
 output_message="Error on $env"
 echo $output_message
 #echo $output_message
-echo "::set-output name=$env_message::$output_message"
+echo "::set-output name=$env'_message'::$output_message"
