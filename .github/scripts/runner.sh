2052bc8c50bc8ee28df27d0a44a3a92c0c34ae68
Oren Zamir <orenzamir5@gmail.com>
2022-07-27 15:45:12 +0300

    Update runner.sh

--- .github/scripts/runner.sh
+++ .github/scripts/runner.sh
@@ -2,7 +2,7 @@
 
 env="$1"
 bla=$env'_var'
-echo "github version: ${env.GITHUB_RUN_NUMBER}"
+echo "github version: ${{env.GITHUB_RUN_NUMBER}}"
 echo $AWS_ACCESS_KEY_ID
 output_message="Error on $env"
 echo $output_message
