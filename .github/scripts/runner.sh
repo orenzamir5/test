b27bbc27ff8feabc130c85dc47526f661965f22f
Oren Zamir <orenzamir5@gmail.com>
2022-10-31 14:57:30 +0200

    Update runner.sh

--- .github/scripts/runner.sh
+++ .github/scripts/runner.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 ####GITHUB_RUN_NUMBER="$1"
+env="$1"
 bla=$env'_var'
-GITHUB_RUN_NUMBER="$1"
-echo "github version: $GITHUB_RUN_NUMBER"
-echo "GITHUB_WORKFLOW: $GITHUB_WORKFLOW"
-echo "GITHUB_RUN_NUMBER: $GITHUB_RUN_NUMBER"
-echo $AWS_ACCESS_KEY_ID
+#GITHUB_RUN_NUMBER="$1"
+#echo "github version: $GITHUB_RUN_NUMBER"
+#echo "GITHUB_WORKFLOW: $GITHUB_WORKFLOW"
+#echo "GITHUB_RUN_NUMBER: $GITHUB_RUN_NUMBER"
+#echo $AWS_ACCESS_KEY_ID
 output_message="Error on $env"
-echo $output_message
+#echo $output_message
 #echo $output_message
 echo "::set-output name=$bla::$output_message"
 #echo "$bla=$output_message" >> $GITHUB_OUTPUT
