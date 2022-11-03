517515c4b56a66c69eccd866b808b76e716e3a93
Oren Zamir <orenzamir5@gmail.com>
2022-11-03 15:58:13 +0200

    Update runner.sh

--- .github/scripts/runner.sh
+++ .github/scripts/runner.sh
@@ -8,14 +8,15 @@ bla=$env'_var'
 #echo "GITHUB_WORKFLOW: $GITHUB_WORKFLOW"
 #echo "GITHUB_RUN_NUMBER: $GITHUB_RUN_NUMBER"
 #echo $AWS_ACCESS_KEY_ID
-output_message="Error on $env yessss"
+#output_message="Error on $env yessss"
 #echo $output_message
 #echo $output_message
 
-output_message=`cat helm_history | sed s'/%0A/\n/'g`
+
 output_message="${output_message//'%'/'%25'}"
 output_message="${output_message//$'\n'/'%0A'}"
 output_message="${output_message//$'\r'/'%0D'}"
+output_message=`cat helm_history | sed 's/%0A/\n/g'`
 echo "$bla=$output_message" >> $GITHUB_OUTPUT
 
 #echo "::set-output name=$bla::$output_message"
