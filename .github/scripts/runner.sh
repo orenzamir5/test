11ba4dc1f2f3feb4fb30758b3540e0e5be453882
Oren Zamir <orenzamir5@gmail.com>
2022-11-16 15:16:36 +0200

    Update runner.sh

--- .github/scripts/runner.sh
+++ .github/scripts/runner.sh
@@ -13,9 +13,9 @@ bla=$env'_var'
 #echo $output_message
 
 output_message=$(cat helm_history)
-output_message=$(echo $output_message | tr '%0A' '/\n')
+#output_message=$(echo $output_message | tr '%0A' '\n')
 #output_message="${output_message//'%'/'%25'}"
-#output_message="${output_message//$'\n'/'%0A'}"
+output_message="${output_message//$'\n'/'%0A'}"
 #output_message="${output_message//$'\r'/'%0D'}"
 
 #output_message=`cat helm_history`
