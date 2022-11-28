212cc161b687e14f6e116d7b5a2270da58577775
Oren Zamir <orenzamir5@gmail.com>
2022-11-28 11:57:27 +0200

    Update runner.sh

--- .github/scripts/runner.sh
+++ .github/scripts/runner.sh
@@ -13,11 +13,14 @@ bla=$env'_var'
 #echo $output_message
 
 output_message=$(cat helm_history)
+echo 'JSON_RESPONSE<<EOF' >> $GITHUB_ENV
+echo $output_message >> $GITHUB_ENV
+echo 'EOF' >> $GITHUB_ENV
 #output_message=$(echo $output_message | tr ' ' '\n')
 #output_message="${output_message//'%'/'%25'}"
 #output_message="${output_message//$'%0A'/'\n'}"
-output_message=$(echo $output_message | sed 's/%0A/\n/g')
-output_message=$(echo $output_message | sed 's/complete/\n complete/g') 
+#output_message=$(echo $output_message | sed 's/%0A/\n/g')
+#output_message=$(echo $output_message | sed 's/complete/\n complete/g') 
 #output_message="${output_message//$'\r'/'%0D'}"
 
 #output_message=`cat helm_history`
@@ -34,6 +37,7 @@ echo "$output_message"
 #echo "$MY_STRING" >> $GITHUB_ENV
 #echo "EOF" >> $GITHUB_ENV
 #echo "${{ process.env.MY_STRING }}"
-echo "$bla=${output_message}" >> $GITHUB_OUTPUT
+#echo "$bla=${output_message}" >> $GITHUB_OUTPUT
+echo "$bla=${output_message}" >> $GITHUB_ENV
 #echo "::set-output name=$bla::$output_message"
 
