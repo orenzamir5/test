797fcfa6f23e4763dbbb142c7fe9262c93b7492b
Oren Zamir <orenzamir5@gmail.com>
2022-12-04 14:22:48 +0200

    Update get-slack-user-id.sh

--- .github/scripts/get-slack-user-id.sh
+++ .github/scripts/get-slack-user-id.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-github_user="$1"
-echo "Author: $github_user"
-echo "SLACK_USER_ID=$( cat slack-user-id.txt | grep ${process.env.AS_AUTHOR} | awk '{print $2}')" >> $GITHUB_ENV
+echo "Author: $AUTHOR"
+echo "SLACK_USER_ID=$(cat slack-user-id.txt | grep $AUTHOR | awk '{print $2}')" >> $GITHUB_ENV
