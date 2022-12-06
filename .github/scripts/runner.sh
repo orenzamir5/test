7ac889de3bba4fc5b0fec3177159724740946e93
Oren Zamir <orenzamir5@gmail.com>
2022-12-06 13:35:42 +0200

    Update runner.sh

--- .github/scripts/runner.sh
+++ .github/scripts/runner.sh
@@ -40,7 +40,9 @@ echo "$output_message"
 #echo 'EOF' >> $GITHUB_OUTPUT
 #echo "$bla=${output_message}" >> $GITHUB_OUTPUT
 
-echo "::set-output name=$bla::$output_message"
+
+#echo "::set-output name=$bla::$output_message"
+#echo "::set-output name=$bla::$output_message"
 
 #echo "$bla<<EOF" >> $GITHUB_OUTPUT
 #echo $output_message >> $GITHUB_OUTPUT
