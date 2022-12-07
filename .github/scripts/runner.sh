1c8f748b32e330ab1ff8f4ec9380edb58571100a
Oren Zamir <orenzamir5@gmail.com>
2022-12-07 16:21:19 +0200

    Update runner.sh

--- .github/scripts/runner.sh
+++ .github/scripts/runner.sh
@@ -40,7 +40,7 @@ echo "$output_message"
 #echo 'EOF' >> $GITHUB_OUTPUT
 #echo "$bla=${output_message}" >> $GITHUB_OUTPUT
 
-output_message="Helm chart version: v0.1.1367 is deployed :white_check_mark:"
+output_message=":white_check_mark: Helm chart version: v0.1.1367 is deployed"
 #echo "::set-output name=$bla::$output_message"
 echo "::set-output name=$bla::$output_message"
 
